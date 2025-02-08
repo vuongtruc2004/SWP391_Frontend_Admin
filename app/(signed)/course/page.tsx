e60cb7fc941c0c04a92f56a1a5c698aaea9251ce
dungtnhe186287 <dungtnhe186287@fpt.edu.vn>
2025-02-08 13:37:39 +0700

    Cập nhật lọc theo trạng thái, tìm theo ngày tạo trong khoảng ngày

--- app/(signed)/course/page.tsx
+++ app/(signed)/course/page.tsx
@@ -6,32 +6,50 @@ import { apiUrl } from "@/utils/url"
 
 const CoursePage = async (props: {
     searchParams: Promise<{
-        keyword?: string;
-        page?: string;
+        keyword: string;
+        page: string;
+        accepted: string;
+        createdFrom: string;
+        createdTo: string;
     }>
 }) => {
+
     const searchParams = await props.searchParams;
     const keyword = searchParams.keyword || ""
     const page = searchParams.page || 1;
+    const accepted = searchParams.accepted || "all"
+    const createdFrom = searchParams.createdFrom || "";
+    const createdTo = searchParams.createdTo || "";
+
     let filter = ""
     if (isFullNumber(keyword)) {
         filter = `courseId : ${keyword}`
     } else {
-        filter = `courseName ~ '${keyword}' or description ~ '${keyword}' or expert.user.fullname ~ '${keyword}'`
+        filter = `(courseName ~ '${keyword}' or description ~ '${keyword}' or expert.user.fullname ~ '${keyword}')`
     }
+
+    if (accepted !== "all") {
+        filter += ` and accepted : ${accepted === "active" ? true : false}`
+    }
+
+    if (createdFrom !== "") {
+        filter += ` and createdAt > '${createdFrom}' and createdAt < '${createdTo}'`
+    }
+
+
     const courseResponse = await sendRequest<ApiResponse<PageDetailsResponse<CourseResponse[]>>>({
-        url: `${apiUrl}/courses`,
+        url: `${apiUrl}/courses/all`,
         queryParams: {
             page: page,
             size: 10,
-            filter: filter
+            filter: filter,
         }
     })
 
     console.log(">>> checkkk", courseResponse)
     return (
         <div className="border w-full h-[85vh] bg-white rounded-lg shadow-[0_0_5px_rgba(0,0,0,0.3)] flex flex-col gap-5">
-            <CourseSearch keyword={keyword} />
+            <CourseSearch keyword={keyword} accepted={accepted} createdFrom={createdFrom} createdTo={createdTo} />
             <CourseTable
                 coursePageResponse={courseResponse.data}
             />
--- components/course/course.search.tsx
+++ components/course/course.search.tsx
@@ -1,18 +1,23 @@
 'use client'
 import { SearchOutlined } from '@ant-design/icons'
-import { Button, Form, Input, Select } from 'antd'
+import { Button, Col, DatePicker, DatePickerProps, Form, Input, InputNumber, InputNumberProps, Row, Select, Slider } from 'antd'
 import { useForm } from 'antd/es/form/Form'
+import { Dayjs } from 'dayjs'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEvent, useEffect, useState } from 'react'
 
-const CourseSearch = (props: { keyword: string, accepted?: string }) => {
-    const { keyword, accepted } = props
+const { RangePicker } = DatePicker;
+const CourseSearch = (props: { keyword: string, accepted?: string, createdFrom?: string, createdTo?: string }) => {
+    const { keyword, accepted, createdFrom, createdTo } = props
     const router = useRouter()
     const searchParam = useSearchParams()
     const pathName = usePathname()
     const [form] = useForm()
+    const [render, setRender] = useState(false);
+    const [inputValue, setInputValue] = useState(1);
 
     const handleReset = () => {
-        form.setFieldsValue({ keyword: '', accepted: undefined })
+        form.setFieldsValue({ keyword: '', accepted: "all", createdFrom: '', createdTo: '' })
         router.push("/course")
     }
 
@@ -26,7 +31,14 @@ const CourseSearch = (props: { keyword: string, accepted?: string }) => {
             newSearchParam.delete("keyword")
         }
 
-        if (accepted !== undefined) {
+        // if (createAt) {
+        //     const formattedDate = createAt.format('YYYY-MM-DD'); // Format lại trước khi thêm vào URL
+        //     newSearchParam.set("createAt", formattedDate);
+        // } else {
+        //     newSearchParam.delete("createAt");
+        // }
+
+        if (accepted !== "all") {
             newSearchParam.set("accepted", accepted)
         } else {
             newSearchParam.delete("accepted")
@@ -36,6 +48,49 @@ const CourseSearch = (props: { keyword: string, accepted?: string }) => {
         router.replace(`${pathName}?${newSearchParam}`)
     }
 
+    const handleChangeStatus = (value: string) => {
+        const newSearchParam = new URLSearchParams(searchParam)
+        newSearchParam.set('accepted', value);
+        newSearchParam.set("page", "1")
+        router.replace(`${pathName}?${newSearchParam}`)
+    }
+
+    const onChangeFrom: DatePickerProps['onChange'] = (date, dateString) => {
+        const newSearchParam = new URLSearchParams(searchParam)
+        if (date) {
+            newSearchParam.set('createdFrom', date.format('YYYY-MM-DD')) // Định dạng chuẩn cho URL
+        } else {
+            newSearchParam.delete('createdFrom')
+        }
+        newSearchParam.set("page", "1")
+        router.replace(`${pathName}?${newSearchParam}`)
+    };
+    const onChangeTo: DatePickerProps['onChange'] = (date, dateString) => {
+        const newSearchParam = new URLSearchParams(searchParam)
+        if (date) {
+            newSearchParam.set('createdTo', date.format('YYYY-MM-DD')) // Định dạng chuẩn cho URL
+        } else {
+            newSearchParam.delete('createdTo')
+        }
+        newSearchParam.set("page", "1")
+        router.replace(`${pathName}?${newSearchParam}`)
+    };
+
+    const onChangeRange: InputNumberProps['onChange'] = (value) => {
+        setInputValue(value as number);
+    }
+
+    useEffect(() => {
+        if (!render) {
+            setRender(true);
+        }
+    }, []);
+
+    if (!render) {
+        return <></>;
+    }
+
+    const dateFormat = 'DD/MM/YYYY'
     return (
         <div className="flex flex-col gap-4 ml-10 mt-16">
             <Form
@@ -51,20 +106,59 @@ const CourseSearch = (props: { keyword: string, accepted?: string }) => {
                         className='!p-[10px]'
                     />
                 </Form.Item>
+                <div>
+                    <Form.Item name="accepted" className="mb-0" label="Trạng Thái">
+                        <Select
 
-                <Form.Item name="accepted" className="mb-0">
-                    <Select
-                        placeholder="Trạng thái"
-                        style={{ width: 150 }}
-                        allowClear
-                        options={[
-                            { value: 1, label: 'Đã kích hoạt' },
-                            { value: 0, label: 'Chưa kích hoạt' }
-                        ]}
-                    />
-                </Form.Item>
+                            placeholder="Trạng thái"
+                            style={{ width: 150 }}
+                            allowClear
+                            onChange={handleChangeStatus}
+                            options={[
+                                { value: "all", label: "Tất cả" },
+                                { value: "active", label: 'Đã kích hoạt' },
+                                { value: "unactive", label: 'Chưa kích hoạt' }
+                            ]}
+                        />
+                    </Form.Item>
+                    <div className='flex gap-4'>
+                        <h4>Ngày Tạo:</h4>
+                        <Form.Item name="createdFrom">
+                            <DatePicker onChange={onChangeFrom} format={dateFormat} placeholder='Từ Ngày' allowClear={false} />
+                        </Form.Item>
+                        <h4>~</h4>
+                        <Form.Item name="createdTo">
+                            <DatePicker onChange={onChangeTo} format={dateFormat} placeholder='Đến Ngày' allowClear={false} />
+                        </Form.Item>
+                    </div>
+                    <Form.Item>
+                        <Row>
+                            <Col span={12}>
+                                <Slider
+                                    min={150000}
+                                    max={950000}
+                                    onChange={onChangeRange}
+                                    value={typeof inputValue === 'number' ? inputValue : 0}
+                                />
+                            </Col>
+                            <Col span={4}>
+                                <InputNumber
+                                    min={1}
+                                    max={20}
+                                    style={{ margin: '0 16px', color: 'black' }}
+                                    value={inputValue}
+                                    onChange={onChangeRange}
+                                    formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " đ"}
 
-                <div className="flex gap-4">
+                                />
+                            </Col>
+                        </Row>
+                    </Form.Item>
+
+
+                </div>
+
+                <div className="flex gap-4 mt-3">
                     <Button type="primary" htmlType="submit" className='!p-[10px]'>Tìm kiếm</Button>
                     <Button onClick={handleReset} className='!p-[10px]'>Làm mới</Button>
                 </div>
